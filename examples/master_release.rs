03a9851076dc75805baac3cbe7f5aca6941ba14e
Simon Persson <simon@flaskpost.org>
2015-08-01 17:22:46 +0000

    Remove unnecessary mut.

--- examples/master_release.rs
+++ examples/master_release.rs
@@ -6,7 +6,7 @@ use std::env;
 fn main()
 {
     if let Some(token) = env::args().nth(1) {
-        let mut discogs = Discogs::new(&token, "TestApp");
+        let discogs = Discogs::new(&token, "TestApp");
         println!("{:#?}", discogs.master_release(62462));
     } else {
         println!("You must provide a token!");
